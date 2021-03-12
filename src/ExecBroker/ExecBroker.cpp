3bc7cd8da2faf38ac7a0be2a61c7025e89868051
Nicolas Surbayrole <nsurbayrole@quarkslab.com>
2021-03-12 15:06:20 +0100

    Fix addInstrumentedModuleFromAddr to work with mmap region

--- src/ExecBroker/ExecBroker.cpp
+++ src/ExecBroker/ExecBroker.cpp
@@ -63,22 +63,27 @@ bool ExecBroker::addInstrumentedModule(const std::string& name) {
 }
 
 bool ExecBroker::addInstrumentedModuleFromAddr(rword addr) {
-    bool instrumented = false;
 
     for(const MemoryMap& m : getCurrentProcessMaps()) {
         if(m.range.contains(addr)) {
-            instrumented = addInstrumentedModule(m.name);
-            break;
+            if (not m.name.empty()) {
+                return addInstrumentedModule(m.name);
+            } else if (m.permission & QBDI::PF_EXEC) {
+                addInstrumentedRange(m.range);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-    return instrumented;
+    return false;
 }
 
 bool ExecBroker::removeInstrumentedModule(const std::string& name) {
     bool removed = false;
 
     for(const MemoryMap& m : getCurrentProcessMaps()) {
-        if((m.name == name) && (m.permission & QBDI::PF_EXEC)) {
+        if (m.name == name) {
             removeInstrumentedRange(m.range);
             removed = true;
         }
@@ -91,11 +96,14 @@ bool ExecBroker::removeInstrumentedModuleFromAddr(rword addr) {
 
     for(const MemoryMap& m : getCurrentProcessMaps()) {
         if(m.range.contains(addr)) {
-            removed = removeInstrumentedModule(m.name);
-            break;
+            removeInstrumentedRange(m.range);
+            if (not m.name.empty()) {
+                removeInstrumentedModule(m.name);
+            }
+            return true;
         }
     }
-    return removed;
+    return false;
 }
 
 bool ExecBroker::instrumentAllExecutableMaps() {
